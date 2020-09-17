d087bb024465ac7b8c85dd904fcdbe9e434ffc4c
ChiragJRana <42936626+ChiragJRana@users.noreply.github.com>
2020-09-17 14:07:44 +0530

    requirements

--- main/videostream/urls.py
+++ main/videostream/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('video_render', views.video_render, name = 'video_render')
+    path('video_render', views.video_render, name = 'video_render'),
 ]
